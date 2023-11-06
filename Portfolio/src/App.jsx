db2db58e38bd95af24edb092f56ad10a4f78d46b
ebrest993 <elliott.brest@gmail.com>
2023-11-06 15:59:05 -0600

    navbar displayed

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,41 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function App() {
-  const [count, setCount] = useState(0)
+// filename -App.js
 
-  return (
-    <>
-      <nav> Elliott
-        <ul>
-          <li src="/">About Me</li>
-          <li src="/portfolio">Portfolio</li>
-          <li>Contact</li>
-          <li>Resume</li>
-        </ul>
-      {/* </Navbar> */}
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </nav>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          This is a 
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <nav> Elliott
+//         <ul>
+//           <li src="/">About Me</li>
+//           <li src="/portfolio">Portfolio</li>
+//           <li>Contact</li>
+//           <li>Resume</li>
+//         </ul>
+//       {/* </Navbar> */}
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </nav>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           This is a 
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
     // <>
     //   <div>
     //     <a href="https://vitejs.dev" target="_blank">
@@ -59,7 +60,7 @@ function App() {
     //     Click on the Vite and React logos to learn more
     //   </p>
     // </>
-  )
-}
+//   )
+// }
 
-export default App
+// export default App
